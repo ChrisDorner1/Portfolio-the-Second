4fe7bf4b69cad3a799a6db87e30b129751eada9b
Chris Spivey <theori373@gmail.com>
2023-09-02 17:04:04 -0700

    more styling

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -4,13 +4,13 @@ const Project = ({project}) => {
   return (
     <div className='col' >
         <div className="card text-bg-dark">
-  <img src={require(`../imgassets/${project.name}.png`)} className="card-img" alt={project.name}/>
+  <img src={require(`../imgassets/${project.name}.png`)} className="card-img blur" alt={project.name}/>
   <div className="card-img-overlay">
-    <h3 className="card-title">
+    <h3 className="card-title mb-3 d-flex justify-content-center">
         <a href={project.link} className="card-text linkColor">{project.name}</a>
-        <a href={project.repo} className="card-text linkColor" ><i className="fab fa-github"></i></a>
+        <a href={project.repo} className="card-text linkColor" ><i className="fab fa-github ms-3"></i></a>
     </h3>
-    <p className="card-text desc">{project.description}</p>
+    <p className="card-text desc d-flex justify-content-center">{project.description}</p>
   </div>
 </div>
     </div>
